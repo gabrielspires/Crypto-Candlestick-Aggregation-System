88bb1d20d5f038e8139cb88133a22972e9b6385c
Gabriel Pires <gabrielhpires@gmail.com>
2021-06-19 16:12:11 -0300

    Adicionado método que extrai o valor do ultimo trade da moeda desejada

--- app/src/candlestick_api.py
+++ app/src/candlestick_api.py
@@ -1,9 +1,11 @@
 import requests
 import json
+from typing import Union
+from datetime import datetime
 
 
 class CandlestickAPI(object):
-    def get_market_data(self, url: str) -> dict:
+    def get_market_data(self, url: str) -> Union[dict, None]:
         """Sends a request for the market data of all currency pairs in the exchange.
 
         Args:
@@ -18,5 +20,38 @@ class CandlestickAPI(object):
 
         if response.ok:
             return json.loads(response.content)
+        else:
+            return None
+
+    def extract_coin_info(self, currency_pair: str) -> Union[dict, None]:
+        """Extracts the last value of the currency pair we want from the api's response and
+        return it with the date and time as the key.
+
+        Args:
+            currency_pair (str): One of the pairs listed at
+            https://docs.poloniex.com/?shell#currency-pair-ids
+
+        Returns:
+            Union[dict, None]: Returns a dictionary with the currency pair as a key with it's value
+            as the execution price of the most recent trade for that pair. If the request fails it
+            returns None.
+        """
+
+        api_command = "returnTicker"
+        api_url = "https://poloniex.com/public?command=" + api_command
+
+        request_answer = None
+        datetime_now = None
+
+        try:
+            datetime_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            request_answer = self.get_market_data("opa")
+        except Exception as e:
+            print("Error with url %s : %s" % (api_url, e))
+
+        if request_answer is not None:
+            response = {datetime_now: float(request_answer[currency_pair]["last"])}
+
+            return response
         else:
             return None
